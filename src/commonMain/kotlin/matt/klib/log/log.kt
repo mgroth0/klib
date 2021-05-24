20de6e590827faa75bc2991b0a32dc501d88ee7f
Matt Groth <mgroth49@gmail.com>
2021-05-24 14:12:27 -0400

    autocommit

--- src/commonMain/kotlin/matt/klib/log/log.kt
+++ src/commonMain/kotlin/matt/klib/log/log.kt
@@ -8,6 +8,18 @@ fun debug(s: Any) {
   }
 }
 
+private val warned = mutableListOf<Any>()
 fun warn(s: Any) {
   println("WARNING:${s.toString().uppercase()}")
+}
+
+fun warnOnce(s: Any) {
+  if (s in warned) return
+  else {
+	warn(s)
+	warned += s
+	if (warned.size > 100) {
+	  throw RuntimeException("too many warnings")
+	}
+  }
 }
