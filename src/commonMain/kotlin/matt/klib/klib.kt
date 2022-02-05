970aabc764907c3405bf794fbd71b292f7e9aa74
Matt Groth <mgroth49@gmail.com>
2022-02-04 22:16:59 -0500

    autocommit

--- src/commonMain/kotlin/matt/klib/klib.kt
+++ src/commonMain/kotlin/matt/klib/klib.kt
@@ -2,18 +2,12 @@ package matt.klib
 
 
 infix fun <K, V> Map<K, V>.isEquivalentTo(other: Map<K, V>?): Boolean {
-  //  println("here1")
   if (other == null) return false
-  //  println("here2")
   if (this.keys.size != other.keys.size) return false
-  //  println("here3")
   this.forEach { (k, v) ->
 	if (k !in other) return false
-	//	println("here4")
 	if (v != other[k]) return false
-	//	println("here5")
   }
-  //  println("here6")
   return true
 }
 
