29e2d4a844df7500724c244b4dc52b6b9b198c33
mgroth0 <mgroth49@gmail.com>
2022-02-06 01:38:50 -0500

    autocommit

--- src/commonMain/kotlin/matt/klib/math/math.kt
+++ src/commonMain/kotlin/matt/klib/math/math.kt
@@ -10,10 +10,14 @@ const val BILLION = THOUSAND*MILLION
 const val TRILLION = THOUSAND*BILLION
 const val QUADRILLION = THOUSAND*TRILLION
 
+fun Int.isEven() = this == 0 || abs(this).mod(2) == 0
+fun Int.isOdd() = !isEven()
+
+fun Float.sq() = pow(2)
+fun Float.cubed() = pow(3)
+
 fun Double.sq() = pow(2)
 fun Double.cubed() = pow(3)
 
-fun Int.isEven() = this == 0 || abs(this).mod(2) == 0
-fun Int.isOdd() = !isEven()
 
 fun DoubleArray.toIntArray() = this.map { it.toInt() }.toIntArray()
