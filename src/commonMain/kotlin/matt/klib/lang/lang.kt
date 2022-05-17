702855a3577408ef00315a26ffa4775bfaf97544
mgroth0 <mgroth49@gmail.com>
2022-05-17 15:11:58 -0400

    autocommit

--- src/commonMain/kotlin/matt/klib/lang/lang.kt
+++ src/commonMain/kotlin/matt/klib/lang/lang.kt
@@ -4,6 +4,11 @@ infix fun Boolean.ifTrue(op: () -> Unit) {
     if (this) op()
 }
 
+
+infix fun <R> Boolean.ifTrueOrNull(op: () -> R): R? {
+   return  if (this) op() else null
+}
+
 infix fun Boolean.ifFalse(op: () -> Unit) {
     if (!this) op()
 }
