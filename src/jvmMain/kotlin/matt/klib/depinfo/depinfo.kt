e3be9c6afd9c170dd0c7618e0333d0d9c80b93ea
mgroth0 <mgroth49@gmail.com>
2022-05-27 18:27:18 -0400

    autocommit

--- src/jvmMain/kotlin/matt/klib/depinfo/depinfo.kt
+++ src/jvmMain/kotlin/matt/klib/depinfo/depinfo.kt
@@ -2,7 +2,23 @@ package matt.klib.depinfo
 
 import kotlinx.serialization.Serializable
 
-@Serializable
-data class DepInfo(val thing: String) {
+@Serializable data class DepInfo(
+  val subprojects: MutableList<SubProject> = mutableListOf()
+)
 
-}
+@Serializable data class SubProject(
+  val folder: String,
+  val configurations: MutableList<Configuration> = mutableListOf()
+)
+
+@Serializable data class Configuration(
+  val name: String,
+  val dependencies: MutableList<Dependency> = mutableListOf()
+)
+
+@Serializable data class Dependency(
+  val group: String,
+  val name: String,
+  val file: String,
+  val dependencies: MutableList<Dependency> = mutableListOf()
+)
