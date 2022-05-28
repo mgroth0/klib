052a2e957affd1d2fbed002567f20295c4cf7ad5
mgroth0 <mgroth49@gmail.com>
2022-05-28 00:39:33 -0400

    autocommit

---
+++ src/commonMain/kotlin/matt/klib/oset/oset.kt
@@ -0,0 +1,109 @@
+package matt.klib.oset
+
+
+fun <E> Collection<E>.toBasicObservableSet(): BasicObservableSet<E> {
+    return BasicObservableSet(this)
+}
+
+fun <E> Iterable<E>.toBasicObservableSet(): BasicObservableSet<E> {
+    return BasicObservableSet(this.toSet())
+}
+
+fun <E> Sequence<E>.toBasicObservableSet(): BasicObservableSet<E> {
+    return BasicObservableSet(this.toSet())
+}
+
+class BasicObservableSet<E>(c: Collection<E>) : MutableSet<E> {
+
+    private val theSet = c.toMutableSet()
+
+
+    private val listeners = mutableListOf<() -> Unit>()
+    fun onChange(op: () -> Unit) {
+        listeners.add(op)
+    }
+
+    private fun change() {
+        listeners.forEach { it() }
+    }
+
+
+    override val size: Int
+        get() = theSet.size
+
+    override fun contains(element: E): Boolean {
+        return theSet.contains(element)
+    }
+
+    override fun containsAll(elements: Collection<E>): Boolean {
+        return theSet.containsAll(elements)
+    }
+
+    override fun isEmpty(): Boolean {
+        return theSet.isEmpty()
+    }
+
+    override fun iterator(): MutableIterator<E> {
+        val mitr = theSet.iterator()
+        return object : MutableIterator<E> {
+            override fun hasNext(): Boolean {
+                return mitr.hasNext()
+            }
+
+            override fun next(): E {
+                return mitr.next()
+            }
+
+            override fun remove() {
+                mitr.remove()
+//                println("set mitr remove")
+                change()
+            }
+        }
+    }
+
+    override fun add(element: E): Boolean {
+        val b = theSet.add(element)
+//        println("BasicObservableSet.add(${element})")
+        if (b) {
+            change()
+        }
+        return b
+    }
+
+    override fun addAll(elements: Collection<E>): Boolean {
+        val b = theSet.addAll(elements)
+//        taball("set addAll",elements)
+        if (b) change()
+        return b
+    }
+
+    override fun clear() {
+//        println("BasicObservableSet.clear")
+        theSet.clear()
+        change()
+    }
+
+    override fun remove(element: E): Boolean {
+        val b = theSet.remove(element)
+//        println("BasicObservableSet.remove(${element})")
+        if (b) change()
+        return b
+    }
+
+    override fun removeAll(elements: Collection<E>): Boolean {
+        val b = theSet.removeAll(elements)
+//        taball("set removeAll",elements)
+        if (b) change()
+        return b
+    }
+
+
+    override fun retainAll(elements: Collection<E>): Boolean {
+        val b = theSet.retainAll(elements)
+//        taball("set retainAll",elements)
+        if (b) change()
+        return b
+    }
+
+}
