986908ebdc61e6f213a23438790811d54b21b573
mgroth0 <mgroth49@gmail.com>
2022-05-28 15:09:02 -0400

    autocommit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 /*NPM INSTALL TASK IS DISABLED IN ROOT BUILDSCRIPT BECAUSE IT PRODUCES OBNOXIOUS WARNING. WILL NEED TO ENABLE THAT TO INSTALL DEPENDENCIES PROBABLY*/
 
+
+
 plugins {
-  kotlin("plugin.serialization") version tomlVersion("kotlin")
+  kotlin("plugin.serialization")
 
   /*experimental.coroutines = org.jetbrains.kotlin.gradle.dsl.Coroutines.ENABLE*/
 }
