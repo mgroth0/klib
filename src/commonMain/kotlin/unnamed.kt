9764916aa96dae3128ce7da9beccf6001409fc73
mgroth0 <mgroth49@gmail.com>
2022-06-10 11:29:32 -0400

    autocommit

--- src/commonMain/kotlin/unnamed.kt
+++ src/commonMain/kotlin/unnamed.kt
@@ -1,1 +1,19 @@
-enum class ModType { APP, CLAPP, APPLIB, LIB, ABSTRACT, ALL, NO_NATIVE }
+sealed interface ModType
+object APP: ModType
+object CLAPP: ModType
+object APPLIB: ModType
+object LIB: ModType
+object ABSTRACT: ModType
+
+sealed interface MultiPlatformMod: ModType /*has common*/
+sealed interface JvmMod: ModType
+sealed interface JsMod: ModType
+sealed interface NativeMod: ModType
+
+object ALL: MultiPlatformMod, JvmMod, JsMod, NativeMod
+object JVM_ONLY: MultiPlatformMod, JvmMod
+object JS_ONLY: MultiPlatformMod, JsMod
+object NATIVE_ONLY: MultiPlatformMod, NativeMod
+object NO_NATIVE: MultiPlatformMod, JvmMod, JsMod
+object NO_JS: MultiPlatformMod, JvmMod, NativeMod
+object NO_JVM: MultiPlatformMod, JsMod, NativeMod
