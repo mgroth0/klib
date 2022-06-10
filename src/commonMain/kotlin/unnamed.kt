82c6d203a2a829046b94e74ad61cc1ef4c716302
mgroth0 <mgroth49@gmail.com>
2022-06-10 18:48:16 -0400

    autocommit

--- src/commonMain/kotlin/unnamed.kt
+++ src/commonMain/kotlin/unnamed.kt
@@ -1,14 +1,17 @@
 sealed interface ModType
-object APP: ModType
-object CLAPP: ModType
-object APPLIB: ModType
-object LIB: ModType
-object ABSTRACT: ModType
-
-sealed interface MultiPlatformMod: ModType /*has common*/
 sealed interface JvmMod: ModType
 sealed interface JsMod: ModType
 sealed interface NativeMod: ModType
+
+object APP: ModType, JvmMod
+object CLAPP: ModType, JvmMod
+object APPLIB: ModType, JvmMod
+object LIB: ModType, JvmMod
+object ABSTRACT: ModType
+
+object JS: JsMod
+
+sealed interface MultiPlatformMod: ModType /*has common*/
 
 object ALL: MultiPlatformMod, JvmMod, JsMod, NativeMod
 object JVM_ONLY: MultiPlatformMod, JvmMod
@@ -17,3 +20,4 @@ object NATIVE_ONLY: MultiPlatformMod, NativeMod
 object NO_NATIVE: MultiPlatformMod, JvmMod, JsMod
 object NO_JS: MultiPlatformMod, JvmMod, NativeMod
 object NO_JVM: MultiPlatformMod, JsMod, NativeMod
+
