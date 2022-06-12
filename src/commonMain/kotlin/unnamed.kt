79105139c160e6f1d32296179cee57d20baddfe4
mgroth0 <mgroth49@gmail.com>
2022-06-12 11:47:04 -0400

    autocommit

--- src/commonMain/kotlin/unnamed.kt
+++ src/commonMain/kotlin/unnamed.kt
@@ -1,6 +1,14 @@
 sealed interface ModType
 sealed interface JvmMod: ModType
-sealed interface JsMod: ModType
+sealed interface JsMod: ModType {
+  val client: Boolean
+}
+sealed interface JsLibOnly: JsMod {
+  override val client get() = false
+}
+sealed interface JsClient: JsMod {
+  override val client get() = true
+}
 sealed interface NativeMod: ModType
 
 object APP: ModType, JvmMod
@@ -9,15 +17,16 @@ object APPLIB: ModType, JvmMod
 object LIB: ModType, JvmMod
 object ABSTRACT: ModType
 
-object JS: JsMod
+object JS_LIB: JsLibOnly
+object JS_CLIENT: JsClient
 
 sealed interface MultiPlatformMod: ModType /*has common*/
 
-object ALL: MultiPlatformMod, JvmMod, JsMod, NativeMod
+object ALL: MultiPlatformMod, JvmMod, JsLibOnly, NativeMod
 object JVM_ONLY: MultiPlatformMod, JvmMod
-object JS_ONLY: MultiPlatformMod, JsMod
+object JS_ONLY: MultiPlatformMod, JsLibOnly
 object NATIVE_ONLY: MultiPlatformMod, NativeMod
-object NO_NATIVE: MultiPlatformMod, JvmMod, JsMod
+object NO_NATIVE: MultiPlatformMod, JvmMod, JsLibOnly
 object NO_JS: MultiPlatformMod, JvmMod, NativeMod
-object NO_JVM: MultiPlatformMod, JsMod, NativeMod
+object NO_JVM: MultiPlatformMod, JsLibOnly, NativeMod
 
