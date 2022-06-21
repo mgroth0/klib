294afd096a6935b60d22e503864ee4bdfba3659d
mgroth0 <mgroth49@gmail.com>
2022-06-20 21:40:35 -0400

    autocommit

--- src/commonMain/kotlin/unnamed.kt
+++ src/commonMain/kotlin/unnamed.kt
@@ -3,13 +3,18 @@ sealed interface JvmMod: ModType
 sealed interface JsMod: ModType {
   val client: Boolean
 }
+
 sealed interface JsLibOnly: JsMod {
   override val client get() = false
 }
+
 sealed interface JsClient: JsMod {
   override val client get() = true
 }
+
 sealed interface NativeMod: ModType
+sealed interface NativeMain: NativeMod
+sealed interface NativeLib: NativeMod
 
 object APP: ModType, JvmMod
 object CLAPP: ModType, JvmMod
@@ -20,13 +25,16 @@ object ABSTRACT: ModType
 object JS_LIB: JsLibOnly
 object JS_CLIENT: JsClient
 
+//object NATIVE_LIB: JsLibOnly
+//object NATIVE_MAIN: JsClient
+
 sealed interface MultiPlatformMod: ModType /*has common*/
 
-object ALL: MultiPlatformMod, JvmMod, JsLibOnly, NativeMod
+object ALL: MultiPlatformMod, JvmMod, JsLibOnly, NativeLib
 object JVM_ONLY: MultiPlatformMod, JvmMod
 object JS_ONLY: MultiPlatformMod, JsLibOnly
-object NATIVE_ONLY: MultiPlatformMod, NativeMod
+object NATIVE_ONLY: MultiPlatformMod, NativeMain
 object NO_NATIVE: MultiPlatformMod, JvmMod, JsLibOnly
-object NO_JS: MultiPlatformMod, JvmMod, NativeMod
-object NO_JVM: MultiPlatformMod, JsLibOnly, NativeMod
+object NO_JS: MultiPlatformMod, JvmMod, NativeLib
+object NO_JVM: MultiPlatformMod, JsLibOnly, NativeLib
 
