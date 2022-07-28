47ceb3972b0ca2d13c484af6bc1a88cbb9351347
Matthew Groth <matthewgroth@Matthews-MacBook-Pro.local>
2022-07-28 09:33:07 -0400

    autocommit

---
+++ src/commonMain/kotlin/matt/klib/release/release.kt
@@ -0,0 +1,36 @@
+package matt.klib.release
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+
+
+
+@Serializable
+class Release(
+  val name: String,
+  val `zipball_url`: String,
+  val `tarball_url`: String,
+  val commit: Commit,
+  val `node_id`: String
+) {
+  val version by lazy { Version(name) }
+}
+
+@Serializable
+class Commit(
+  val sha: String,
+  val url: String
+)
+
+@Serializable
+data class Version(val first: Int, val second: Int, val third: Int): Comparable<Version> {
+  constructor(versionString: String): this(
+	versionString.split(".")[0].toInt(), versionString.split(".")[1].toInt(), versionString.split(".")[2].toInt()
+  )
+
+  override operator fun compareTo(other: Version): Int {
+	return (first.compareTo(other.first))
+	  .takeIf { it != 0 } ?: ((second.compareTo(other.second)).takeIf { it != 0 } ?: (third.compareTo(other.third)))
+  }
+}
